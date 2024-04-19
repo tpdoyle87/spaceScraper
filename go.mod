1e64ef02edc03efd17d83b0d69f76303296c01cd
Thomas DoyleEngler <tpdoyle87@gmail.com>
2024-04-18 20:11:25 -0400

    add initial database connection and create unique hash per article

--- go.mod
+++ go.mod
@@ -12,8 +12,10 @@ require (
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.24.0 // indirect
--- main.go
+++ main.go
@@ -1,68 +1,81 @@
 package main
 
 import (
-	"encoding/csv"
+	"crypto/sha256"
+	"database/sql"
+	"fmt"
 	"github.com/gocolly/colly"
-	"os"
+	_ "github.com/lib/pq"
 )
 
 type Article struct {
+	Id      string
 	Title   string
-	Link    string
+	URL     string
 	Company string
 	Date    string
+	Hash    string
 }
 
 // article.Title = e.ChildText(".article-name")
 func main() {
-	file, err := os.Create("articles.csv")
+	connStr := "user=tom dbname=scraper sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	headers := []string{"Company", "Title", "Link", "Date"}
-	writer.Write(headers)
-
+	defer db.Close()
 	urls := []string{"https://www.space.com/", "https://www.seti.org/news"}
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.Async(true),
+	)
 	article := Article{}
 	for _, url := range urls {
 		switch url {
 		case "https://www.space.com/":
 			c.OnHTML(".article-link", func(e *colly.HTMLElement) {
-				WriteSpace(e, article, writer)
+				WriteSpace(e, article, db)
 			})
 		case "https://www.seti.org/news":
 			c.OnHTML("div[class=row]", func(e *colly.HTMLElement) {
 				e.ForEach(".col-md-3", func(_ int, e *colly.HTMLElement) {
-					WriteSeti(article, e, writer)
+					WriteSeti(article, e, db)
 				})
 			})
 		}
 		c.Visit(url)
+		c.Wait()
 	}
 }
 
-func WriteSpace(e *colly.HTMLElement, article Article, writer *csv.Writer) {
+func WriteSpace(e *colly.HTMLElement, article Article, db *sql.DB) {
 	article.Company = "space.com"
 	article.Title = e.ChildText(".article-name")
-	article.Link = e.Attr("href")
-	err := writer.Write([]string{article.Company, article.Title, article.Link, article.Date})
-	if err != nil {
-		panic(err)
-	}
+	article.URL = e.Attr("href")
+
+	toHash := article.Company + article.Title + article.URL
+	article.Hash = byteArrayToString(sha256.Sum256([]byte(toHash)))
+	UpsertArticles(db, article)
+
 }
 
-func WriteSeti(article Article, e *colly.HTMLElement, writer *csv.Writer) {
+func WriteSeti(article Article, e *colly.HTMLElement, db *sql.DB) {
 	article.Company = "seti.org"
 	article.Title = e.ChildText("a")
-	article.Link = "https://www.seti.org" + e.ChildAttr("a", "href")
+	article.URL = "https://www.seti.org" + e.ChildAttr("a", "href")
 	article.Date = e.ChildText(".views-field-field-date")
-	err := writer.Write([]string{article.Company, article.Title, article.Link, article.Date})
+
+	toHash := article.Company + article.Title + article.URL
+	article.Hash = byteArrayToString(sha256.Sum256([]byte(toHash)))
+	UpsertArticles(db, article)
+}
+
+func byteArrayToString(b [32]byte) string {
+	return fmt.Sprintf("%x", b)
+}
+
+func UpsertArticles(db *sql.DB, article Article) {
+	_, err := db.Exec("INSERT INTO articles(company, title, url, date, hash) VALUES($1, $2, $3, $4, $5) ON CONFLICT (hash) DO NOTHING", article.Company, article.Title, article.URL, article.Date, article.Hash)
 	if err != nil {
 		panic(err)
 	}
